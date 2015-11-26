880d5b0cca1fade6799147a212b7b4d5534144a2
meerkqat <jurijslabanja@hotmail.com>
2015-11-26 11:44:35 +0100

    cleaned up Test a bit

--- src/Test.java
+++ src/Test.java
@@ -14,65 +14,86 @@ import org.gstreamer.State;
 import org.gstreamer.elements.PlayBin2;
 import org.gstreamer.swing.VideoComponent; 
 public class Test { 
-    private static Pipeline pipe;
-    private static PlayBin2 pipe2;
+    private static Pipeline outPipe;
+    private static Pipeline inPipe;
+    private static PlayBin2 streamPipe;
     public static void main(String[] args) { 
         args = Gst.init("SwingVideoTest", args); 
-        pipe = new Pipeline("pipeline"); 
-        pipe2 = new PlayBin2("p2");
+        outPipe = new Pipeline("outPipe"); 
+        inPipe = new Pipeline("inPipe"); 
+        streamPipe = new PlayBin2("streamPipe");
         
-        final Element videosrc = ElementFactory.make("v4l2src", "vidsrc"); 
-        final Element videofilter = ElementFactory.make("capsfilter", "flt"); 
-        videofilter.setCaps(Caps.fromString("video/x-raw-yuv, width=640, height=480")); 
-        final Element tee = ElementFactory.make("tee", "tee0");
-        tee.set("silent", "false");
         
-        /*
-        // video from file element
-        final Element filesrc = ElementFactory.make("filesrc", "filesrc");
-        filesrc.set("location", "/home/jurij/Downloads/test.mp4");
-        */
+//        final Element videosrc = ElementFactory.make("v4l2src", "vidsrc"); 
+//        final Element videofilter = ElementFactory.make("capsfilter", "flt"); 
+//        videofilter.setCaps(Caps.fromString("video/x-raw-yuv, width=640, height=480")); 
+//        final Element tee = ElementFactory.make("tee", "tee0");
+//        tee.set("silent", "false");
+//        
+//        /*
+//        // video from file element
+//        final Element filesrc = ElementFactory.make("filesrc", "filesrc");
+//        filesrc.set("location", "/home/jurij/Downloads/test.mp4");
+//        */
         
-        /*
-        // udpsink doesn't work like this probably 
-        final Element encoder = ElementFactory.make("jpegenc", "encoder");
-        final Element payloader = ElementFactory.make("rtpjpegpay", "payloader");
-        final Element sink = ElementFactory.make("udpsink", "sink");
-	    sink.set("host", "127.0.0.1");
-	    sink.set("port", "45001");
-	    sink.set("sync", "true");
-	   `*/
+        // stream video over UDP
+        // gst-launch v4l2src device=/dev/video0 ! 'video/x-raw-yuv,width=640,height=480' !  x264enc pass=qual quantizer=20 tune=zerolatency ! rtph264pay ! udpsink host=127.0.0.1 port=1234
+        final Element v4l2src = ElementFactory.make("v4l2src","videosrc");
+        final Element filter = ElementFactory.make("capsfilter","filter");
+        filter.setCaps(Caps.fromString("video/x-raw-yuv, width=640, height=480")); 
+        final Element x264enc = ElementFactory.make("x264enc","encoder");
+        //x264enc.set("pass", "qual");
+        x264enc.set("quantizer", "20");
+        //x264enc.set("tune", "zerolatency");
+        final Element rtph264pay = ElementFactory.make("rtph264pay","payloader");
+        final Element udpsink = ElementFactory.make("udpsink","sink");
+        udpsink.set("host", "127.0.0.1");
+	    udpsink.set("port", "1234");
+
+	    // receive video stream over UDP
+        // gst-launch udpsrc port=1234 ! "application/x-rtp, payload=127" ! rtph264depay ! ffdec_h264 ! xvimagesink sync=false
+	    final Element udpsrc = ElementFactory.make("udpsrc", "udpsrc");
+	    udpsrc.set("port", "1234");
+	    final Element filter2 = ElementFactory.make("capsfilter", "filter2");
+	    filter.setCaps(Caps.fromString("application/x-rtp, payload=127")); 
+	    final Element rtph264depay = ElementFactory.make("rtph264depay", "payldr");
+	    final Element ffdec_h264 = ElementFactory.make("ffdec_h264", "decoder");
+	    //final Element xvsink = ElementFactory.make("xvimagesink", "xvsink");
+	    //xvsink.set("sync", "false");
 
         SwingUtilities.invokeLater(new Runnable() { 
             public void run() { 
-                VideoComponent videoComponent = new VideoComponent(); 
+            	VideoComponent videoComponent = new VideoComponent(); 
                 Element videosink = videoComponent.getElement(); 
-                videosink.setName("a");
+                videosink.setName("center");
+
                 VideoComponent videoComponent2 = new VideoComponent();
                 Element videosink2 = videoComponent2.getElement();
-                videosink2.setName("b");
- 
-                /*
-               `//split stream to multiple sinks
-                pipe.addMany(videosrc, videofilter, videosink, tee, videosink2); 
-                videosrc.link(tee);
-                videofilter.link(tee);
-                tee.link(videosink);
-                tee.link(videosink2);
-                */
+                videosink2.setName("south");
+
                 
-                // get stream from net 
-                pipe.addMany(videosrc, videofilter, videosink);
-                videosrc.link(videofilter);
-                videofilter.link(videosink);
-                //pipe2.setInputFile(new File("/home/jurij/Downloads/test.mp4"));
+//                //split stream to multiple sinks
+//                inPipe.addMany(videosrc, videofilter, videosink, tee, videosink2); 
+//                videosrc.link(tee);
+//                videofilter.link(tee);
+//                tee.link(videosink);
+//                tee.link(videosink2);
+                
+
+                outPipe.addMany(v4l2src, filter, x264enc, rtph264pay, udpsink);
+                Element.linkMany(v4l2src, filter, x264enc, rtph264pay, udpsink);
+                
+                inPipe.addMany(udpsrc, filter2, rtph264depay, ffdec_h264, videosink);
+                Element.linkMany(udpsrc, filter2, rtph264depay, ffdec_h264, videosink);
+
+                //streamPipe.setInputFile(new File("/home/jurij/Downloads/test.mp4"));
                 try {
-                	pipe2.setURI(new URI("http://1tv.ambra.ro"));
+                	streamPipe.setURI(new URI("http://1tv.ambra.ro"));
                 }
                 catch (Exception e) {
                 	e.printStackTrace();
                 }
-                pipe2.setVideoSink(videosink2);
+                streamPipe.setVideoSink(videosink2);
                 
                 JFrame frame = new JFrame("Swing Video Test"); 
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
@@ -82,10 +103,17 @@ public class Test {
                 videoComponent2.setPreferredSize(new Dimension(640, 480));
                 frame.pack(); 
                 frame.setVisible(true); 
-                // Start the pipeline processing 
-                pipe.setState(State.PLAYING); 
-                pipe2.setState(State.PLAYING);
+                
+                outPipe.setState(State.PLAYING);
+                inPipe.setState(State.PLAYING);
+                streamPipe.setState(State.PLAYING);
             } 
         }); 
+        
+        Gst.main();
+		outPipe.setState(State.NULL);
+        inPipe.setState(State.NULL);
+		streamPipe.setState(State.NULL);
+		
     }
 }
