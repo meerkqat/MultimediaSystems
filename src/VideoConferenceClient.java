d05edf8dd967025e6aad9c1b0eeed0ea0624c2bd
Aragnos <dominikGils@gmail.com>
2015-12-08 20:24:51 +0100

    But buffer gets filled

--- src/VideoConferenceClient.java
+++ src/VideoConferenceClient.java
@@ -8,6 +8,15 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+
+import org.gstreamer.Buffer;
+import org.gstreamer.Caps;
+import org.gstreamer.Element;
+import org.gstreamer.ElementFactory;
+import org.gstreamer.Gst;
+import org.gstreamer.Pipeline;
+import org.gstreamer.elements.AppSink;
 
 public class VideoConferenceClient {
 	public String multicastAddress = "232.2.3.2:2345";
@@ -16,109 +25,147 @@ public class VideoConferenceClient {
 	private BufferedReader in;
 	private VideoConferenceGUI gui;
 	private Thread listenLoop;
-	
-	
-	
-	public VideoConferenceClient (String[] args) {
-		// start multicast 
+	private AppSink appsink = (AppSink) ElementFactory.make("appsink",
+			"appsink");
+	private Pipeline outPipe;
+
+	public VideoConferenceClient(String[] args) {
+		// start multicast
 		Thread camStreamer = new Streamer(multicastAddress);
 		camStreamer.start();
-		
+
 		// start gui
-		gui = new VideoConferenceGUI(this,args);		
+		gui = new VideoConferenceGUI(this, args);
 	}
-	
+
 	// on join channel
 	public void join(String serverAddress) {
-		System.out.println("Joining server on "+serverAddress);
-		
+		System.out.println("Joining server on " + serverAddress);
+
 		try {
 			String[] banana = serverAddress.split(":");
-		    socket = new Socket(banana[0], Integer.valueOf(banana[1]));
-		    out = new PrintWriter(socket.getOutputStream(), true);
-		    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		}
-		catch (IOException e) {
+			socket = new Socket(banana[0], Integer.valueOf(banana[1]));
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+		} catch (IOException e) {
 			System.out.println("Error opening socket!");
 			e.printStackTrace();
 			return;
 		}
-		
+
 		// publish our stream address
-		out.write(multicastAddress+"\n");
+		out.write(multicastAddress + "\n");
 		out.flush();
 		System.out.println("Sent multicast address");
-		
+
 		// subscribe to other streams
-		if (listenLoop != null) listenLoop.interrupt(); 
+		if (listenLoop != null)
+			listenLoop.interrupt();
 		listenLoop = new ServerListener();
 		listenLoop.start();
 	}
-	
+
 	// listens for new multicast addresses coming from the server
 	private class ServerListener extends Thread {
 		@Override
 		public void run() {
 			System.out.println("Starting to listen to server...");
 			String line = "";
-			while("pigs" != "fly") {
+			while ("pigs" != "fly") {
 				try {
 					line = in.readLine();
-				}
-				catch (IOException e) {
+				} catch (IOException e) {
 					System.out.println("Error writing to socket!");
 					listenLoop = null;
 					interrupt();
 				}
-				
-				gui.addNewStream(line);  
+
+				gui.addNewStream(line);
 			}
 		}
 	}
-	
+
 	private class Streamer extends Thread {
 		DatagramSocket socket;
 		InetAddress host;
 		int port;
-		
+
 		byte[] outBuf;
-		
+
 		public Streamer(String streamTo) {
 			System.out.println("Init cam streamer");
 			String[] banana = streamTo.split(":");
 			try {
 				host = InetAddress.getByName(banana[0]);
-			}
-			catch (UnknownHostException e) {
+			} catch (UnknownHostException e) {
 				System.out.println("Error getting multicast address!");
 				e.printStackTrace();
 			}
 			port = Integer.valueOf(banana[1]);
-			
+
 			try {
 				socket = new DatagramSocket();
-			}
-			catch (SocketException e) {
+			} catch (SocketException e) {
 				System.out.println("Error opening multicast socket!");
 				e.printStackTrace();
 			}
-			
+
 			outBuf = new byte[42]; // TODO
 		}
-		
+
 		@Override
 		public void run() {
-			if (socket == null || host == null) interrupt();
+			// init appsink
+			// Appsink
+
+			final Element v4l2src = ElementFactory.make("v4l2src", "v4l2src");
+			final Element filter = ElementFactory.make("capsfilter", "filter");
+			filter.setCaps(Caps.fromString(String.format(
+					"video/x-raw-yuv, width=%s, height=%s"
+							+ ", bpp=24, depth=16,framerate=%s/1", "640",
+					"480", "30")));
+			final Element encoder = ElementFactory.make("theoraenc", "encoder");
+			final Element formatConverter = ElementFactory.make(
+					"ffmpegcolorspace", "formatConverter");
+			final Element muxer = ElementFactory.make("oggmux", "muxer");
+			final Element ratefilter = ElementFactory.make("capsfilter",
+					"ratefilter");
+			ratefilter.setCaps(Caps.fromString(String.format(
+					"video/x-raw-yuv,framerate=%s/1", "30")));
+
+			appsink.set("emit-signals", true);
+			appsink.setSync(false);
+			appsink.connect(new AppSink.NEW_BUFFER() {
+
+				@Override
+				public void newBuffer(AppSink arg0) {
+					Buffer buffer = arg0.getLastBuffer();
+					ByteBuffer byteBuffer = buffer.getByteBuffer();
+					outBuf = new byte[byteBuffer.remaining()];
+					byteBuffer.get(outBuf);
+				}
+			});
+			outPipe = new Pipeline("outPipe");
+			outPipe.addMany(v4l2src, formatConverter, filter, encoder, muxer,
+					appsink);
+			Element.linkMany(v4l2src, formatConverter, filter, encoder, muxer,
+					appsink);
+			outPipe.setState(org.gstreamer.State.PLAYING);
+			if (socket == null || host == null)
+				interrupt();
 
 			System.out.println("Streaming webcam...");
-			
+
 			try {
 				DatagramPacket outPacket;
 				while (true) {
 					// TODO fill outBuf from pipeline
-
-					//Send to multicast IP:port
-					outPacket = new DatagramPacket(outBuf, outBuf.length, host, port);
+					System.out.println(outBuf);
+					System.out.println(outBuf.length);
+					// Send to multicast IP:port
+					outPacket = new DatagramPacket(outBuf, outBuf.length, host,
+							port);
 					socket.send(outPacket);
 				}
 			} catch (IOException e) {
@@ -127,8 +174,9 @@ public class VideoConferenceClient {
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
+		args = Gst.init("SwingVideoTest", args);
 		new VideoConferenceClient(args);
 	}
 }
